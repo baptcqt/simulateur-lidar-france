459e6141543b82670b16b6e1120438299848bbf3
baptcqt <baptistecqt988@gmail.com>
2026-07-28 19:51:01 +0200

    Transmettre les CRS au décodeur COPC local

--- web/src/local-las-parser.ts
+++ web/src/local-las-parser.ts
@@ -1,5 +1,12 @@
 import * as THREE from 'three';
+import proj4 from 'proj4';
 import { spawn, Thread, Transfer } from 'threads';
+
+const PROJECTION_DEFINITIONS: Record<string, string> = {
+  'EPSG:2154': '+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs',
+  'EPSG:4326': '+proj=longlat +datum=WGS84 +no_defs +type=crs',
+  'EPSG:4978': '+proj=geocent +datum=WGS84 +units=m +no_defs +type=crs',
+};
 
 type DecodedAttributes = {
   position: Float32Array;
@@ -40,6 +47,15 @@ async function getDecoder(): Promise<DecoderThread> {
   return decoderPromise;
 }
 
+function projectionDefinition(crs?: string): string | Record<string, unknown> | undefined {
+  if (!crs) return undefined;
+  if (PROJECTION_DEFINITIONS[crs]) return PROJECTION_DEFINITIONS[crs];
+
+  const definition = proj4.defs(crs);
+  if (!definition) return undefined;
+  return JSON.parse(JSON.stringify(definition)) as Record<string, unknown>;
+}
+
 function buildBufferGeometry(attributes: DecodedAttributes): THREE.BufferGeometry {
   const geometry = new THREE.BufferGeometry();
   geometry.setAttribute('position', new THREE.BufferAttribute(attributes.position, 3));
@@ -61,11 +77,18 @@ function buildBufferGeometry(attributes: DecodedAttributes): THREE.BufferGeometr
 
 export async function parseLocalLasChunk(data: ArrayBuffer, options: any = {}): Promise<THREE.BufferGeometry> {
   const decoder = await getDecoder();
+  const sourceCrs = options.in?.crs as string | undefined;
+  const targetCrs = options.out?.crs as string | undefined;
+
   const parsedData = await decoder.parseChunk(Transfer(data), {
     pointCount: options.in?.pointCount,
     header: options.in?.header,
     eb: options.in?.eb,
     colorDepth: options.in?.colorDepth,
+    sourceCrs,
+    targetCrs,
+    sourceDefinition: projectionDefinition(sourceCrs),
+    targetDefinition: projectionDefinition(targetCrs),
   });
   return buildBufferGeometry(parsedData.attributes);
 }
