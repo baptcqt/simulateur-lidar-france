cf45024451fffb076ee5e2b8ca0275b2083f334d
baptcqt <baptistecqt988@gmail.com>
2026-07-31 12:08:39 +0200

    Monter le frontend sur le serveur API

--- server/main.py
+++ server/main.py
@@ -6,8 +6,10 @@ from __future__ import annotations
 import sitecustomize  # noqa: F401
 
 from server.app import app
+from server.frontend import mount_frontend
 from server.instance_identity import router as runtime_identity_router
 from server.observability import install_observability
 
 app.include_router(runtime_identity_router)
 install_observability(app)
+mount_frontend(app)
